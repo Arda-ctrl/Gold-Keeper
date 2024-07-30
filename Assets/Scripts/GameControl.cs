bee0f77747bb1af6ef26e826cde8f7e4d4c3f098
Arda Cihat Karahan <ardacihatkarahan@gmail.com>
2024-07-31 00:16:58 +0300

    Map edits
    Adding some golds and fixed collieders

--- Assets/Scripts/GameControl.cs
+++ Assets/Scripts/GameControl.cs
@@ -8,12 +8,22 @@ public class GameControl : MonoBehaviour
     public int coinCount = 0;
     public UnityEngine.UI.Text coinCountText;
     public GameObject hiddenCube;
+    public GameObject hiddenCube1;
+    public GameObject hiddenCube2;
+    public GameObject hiddenCube3;
+    public GameObject hiddenCube4;
+    public GameObject hiddenCube5;
     // Start is called before the first frame update
     void Start()
     {
         if (hiddenCube != null)
         {
             SetObjectVisible(hiddenCube, false); // Baþlangýçta küpü görünmez yapar
+            SetObjectVisible(hiddenCube1, false);
+            SetObjectVisible(hiddenCube2, false);
+            SetObjectVisible(hiddenCube3, false);
+            SetObjectVisible(hiddenCube4, false);
+            SetObjectVisible(hiddenCube5, false);
         }
         else
         {
@@ -25,9 +35,13 @@ public class GameControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (coinCount >= 1 && hiddenCube != null)
+        if (coinCount == 2 && hiddenCube != null)
         {
             SetObjectVisible(hiddenCube, true); // Küpü görünür hale getirir
+        }
+        if (coinCount == 4 && hiddenCube1 != null)
+        {
+            SetObjectVisible(hiddenCube1, true);
         }
     }
     public void Coinincrease()
